1e2bb040feb4e7f0870801b79b95aedc24370853
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-08-01 12:01:47 +0300

    Fixe some Modal

--- script.js
+++ script.js
@@ -1,61 +1,62 @@
 console.log('HELLO');
 
 const recipes = [
-	{
-		title: 'Eggs',
-		picture: 'https://bit.ly/2ZXyiKI',
-		author: 'Loïc',
-		difficulty: 'easy',
-		timing: '15',
-		ingredients: ['eggs', 'salt', 'water'],
-		steps: [
-			'Put a pan on the fire',
-			'Crack the eggs on it',
-			'Wait, put them out',
-			'Add some salt on it',
-		],
-		id: 1596168482053,
-	},
-	{
-		title: 'Eggs',
-		picture: 'https://bit.ly/2ZXyiKI',
-		author: 'Loïc',
-		difficulty: 'easy',
-		timing: '15',
-		ingredients: ['eggs', 'salt', 'water'],
-		steps: [
-			'Put a pan on the fire',
-			'Crack the eggs on it',
-			'Wait, put them out',
-			'Add some salt on it',
-		],
-		id: 1596168522409,
-	},
-	{
-		title: 'My recipe',
-		picture: 'https://bit.ly/2ZXyiKI',
-		author: 'Loïc',
-		difficulty: 'easy',
-		timing: '15',
-		ingredients: ['eggs', 'salt', 'water'],
-		steps: [
-			'Put a pan on the fire',
-			'Crack the eggs on it',
-			'Wait, put them out',
-			'Add some salt on it',
-		],
-		id: 1596168522409,
-	},
+  {
+    title: 'Eggs',
+    picture: 'https://bit.ly/2ZXyiKI',
+    author: 'Loïc',
+    difficulty: 'easy',
+    timing: '15',
+    ingredients: ['eggs', 'salt', 'water'],
+    steps: [
+      'Put a pan on the fire',
+      'Crack the eggs on it',
+      'Wait, put them out',
+      'Add some salt on it',
+    ],
+    id: 1596168482053,
+  },
+  {
+    title: 'Eggs',
+    picture: 'https://bit.ly/2ZXyiKI',
+    author: 'Loïc',
+    difficulty: 'easy',
+    timing: '15',
+    ingredients: ['eggs', 'salt', 'water'],
+    steps: [
+      'Put a pan on the fire',
+      'Crack the eggs on it',
+      'Wait, put them out',
+      'Add some salt on it',
+    ],
+    id: 1596168522409,
+  },
+  {
+    title: 'My recipe',
+    picture: 'https://bit.ly/2ZXyiKI',
+    author: 'Loïc',
+    difficulty: 'easy',
+    timing: '15',
+    ingredients: ['eggs', 'salt', 'water'],
+    steps: [
+      'Put a pan on the fire',
+      'Crack the eggs on it',
+      'Wait, put them out',
+      'Add some salt on it',
+    ],
+    id: 1596168522409,
+  },
 ];
 
 const renderCard = () => {
-	// check the recipes collection 
-	const contain = document.querySelector(".container");
 
-	// generate the HTML
-	for(let i = 0; i < recipes.length; i++ ) {
+  // check the recipes collection 
+  const contain = document.querySelector(".container");
 
-	const myHTML = `<div class="recipe_info">
+  // generate the HTML
+  for (let i = 0; i < recipes.length; i++) {
+
+    const myHTML = `<div class="recipe_info">
 		<h4>${recipes[i].title}</h4>
     <img class="image_for_recipe" src="${recipes[i].picture}" alt="favorite food">
     <ul class="list_of_info">
@@ -66,64 +67,102 @@ const renderCard = () => {
     </ul>
     <button class="more-info">More info</button>
 	</div>`;
-	
-	
-	// put it in the DOM
 
-	contain.insertAdjacentHTML('beforeend', myHTML);
-	};
+
+    // put it in the DOM
+
+    contain.insertAdjacentHTML('beforeend', myHTML);
+  };
 
 
 };
+const outerModal = document.querySelector('.outer-modal');
+const innerModal = document.querySelector('.inner-modal');
+const detail = document.querySelector('.detail_for_recipe');
 
 // Function for the modal
 
 const detailRecipe = () => {
-	const contain = document.querySelector(".container");
+  const contain = document.querySelector(".container");
+  // Create an html
 
-	// Create an html
+  for (let i = 0; i < recipes.length; i++){
 
-	detailHTML = `
-	     <div class="detail_for_recipe">
-      <div class="heading_detail">
-        <h4>Recipe name </h4>
-        <p>authore</p>
-      </div>
-      <ul class="list_of_some_info">
-        <li>Difficult</li>
-        <li>Timing</li>
-      </ul>
-      <ul class="list_of_info">
-        <li>Steps
-          <ol>
-            <li>Do this</li>
-            <li>Do that</li>
-            <li>And do this and that</li>
-            <li></li>
-          </ol>
-        </li>
-        <li>Ingredients List
-          <ul>
-            <li>ing1</li>
-            <li>ing2</li>
-            <li>1ng3</li>
-          </ul>
-        </li>
-      </ul>
-      <img src="" alt="">
+  detailHTML = `
+<div class="detail_for_recipe">
+  <div class="heading_detail">
+    <h2>${recipes[i].title}  <small>${recipes[i].author}</small></h2>
+  </div>
+  <img src="https://bit.ly/2ZXyiKI" alt="">
+  <ul class="list_of_some_info">
+    <li>${recipes[i].difficulty}</li>
+    <li>${recipes[i].timing}</li>
+  </ul>
+  <ul class="list_of_info">
+    <li>Ingredient
+      <ol>
+        <li>${recipes[i].ingredients[0]}</li>
+        <li>${recipes[i].ingredients[1]}</li>
+        <li>${recipes[i].ingredients[2]}</li>
+      </ol>
+    </li>
+    <li>Steps
       <ul>
+        <li>${recipes[i].steps[0]}</li>
+        <li>${recipes[i].steps[1]}</li>
+        <li>${recipes[i].steps[2]}</li>
+        <li>${recipes[i].steps[3]}</li>
       </ul>
-
-    </div>`;
-	
-	innerModal.innerHTML = orderHTML;
-	outerModal.classList.add('open');
+    </li>
+  </ul>`;
+  };
 
 
+  // contain.insertAdjacentHTML('beforeend', detailHTML)
+
+  innerModal.innerHTML += detailHTML;
+  // Add a new class list so that we can creat function to close the the model
+  outerModal.classList.add('open');
+};
+
+// Close model when we don't need the model
+
+const handleCloseModal = event => {
+  const isOutside = !event.target.closest('.inner-modal');
+  if (isOutside) {
+    closeModal();
+  }
+};
+
+// 
+
+const closeModal = () => {
+  outerModal.classList.remove('open');
+};
+
+const handleEscapeKey = event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+};
+
+// Fuction to generate the event Listener of the button of information
+
+const handleClick = (e) => {
+
+  // This condition for button clicked 
+
+  if (e.target.matches('.more-info')) {
+    detailRecipe();
+  }
+  if (e.target.matches('.more-info')){
+     const detail = e.target.closest('');
+     const id = Number(detail.dataset.id);
+     const realRecep = rrecipes.find(realRecep => realRecep.id === id)
+  }
 }
-
 const generateButton = document.querySelector('button.generate');
 generateButton.addEventListener('click', renderCard);
-const generateBtInfo = document.querySelector('button.more-info');
-generateBtInfo.addEventListener('click', detailRecipe);
+window.addEventListener('click', handleClick);
+outerModal.addEventListener('click', handleCloseModal);
 
