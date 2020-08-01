6dde8ebceb91dcff5883500ddde059f116644c23
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-08-01 16:12:01 +0300

    Chages

--- script.js
+++ script.js
@@ -48,121 +48,125 @@ const recipes = [
   },
 ];
 
-const renderCard = () => {
+    const renderCard = () => {
 
-  // check the recipes collection 
-  const contain = document.querySelector(".container");
+// check the recipes collection 
+      const contain = document.querySelector(".container");
 
   // generate the HTML
-  for (let i = 0; i < recipes.length; i++) {
+      for (let i = 0; i < recipes.length; i++) {
 
-    const myHTML = `<div class="recipe_info">
-		<h4>${recipes[i].title}</h4>
-    <img class="image_for_recipe" src="${recipes[i].picture}" alt="favorite food">
-    <ul class="list_of_info">
-      <li class="information_difficulty" >${recipes[i].difficulty}
-      </li>
-      <li class="information_timing"> ${recipes[i].timing}
-      </li>
-    </ul>
-    <button class="more-info">More info</button>
-	</div>`;
+        const myHTML = `<div class="recipe_info">
+	      	<h4>${recipes[i].title}</h4>
+            <img class="image_for_recipe" src="${recipes[i].picture}" alt="favorite food">
+            <ul class="list_of_info">
+              <li class="information_difficulty" >${recipes[i].difficulty}
+              </li>
+              <li class="information_timing"> ${recipes[i].timing}
+              </li>
+            </ul>
+            <button class="more-info">More info</button>
+	      </div>`;
 
 
-    // put it in the DOM
+  // put it in the DOM
 
     contain.insertAdjacentHTML('beforeend', myHTML);
   };
-
-
 };
-const outerModal = document.querySelector('.outer-modal');
-const innerModal = document.querySelector('.inner-modal');
-const detail = document.querySelector('.detail_for_recipe');
+
+// Grabe some element
+
+    const outerModal = document.querySelector('.outer-modal');
+    const innerModal = document.querySelector('.inner-modal');
+    const detail = document.querySelector('.detail_for_recipe');
 
 // Function for the modal
 
-const detailRecipe = () => {
-  const contain = document.querySelector(".container");
+  const detailRecipe = () => {
+    const contain = document.querySelector(".container");
   // Create an html
 
-  for (let i = 0; i < recipes.length; i++){
+    for (let i = 0; i < recipes.length; i++) {
 
-  detailHTML = `
-<div class="detail_for_recipe">
-  <div class="heading_detail">
-    <h2>${recipes[i].title}  <small>${recipes[i].author}</small></h2>
-  </div>
-  <img src="https://bit.ly/2ZXyiKI" alt="">
-  <ul class="list_of_some_info">
-    <li>${recipes[i].difficulty}</li>
-    <li>${recipes[i].timing}</li>
-  </ul>
-  <ul class="list_of_info">
-    <li>Ingredient
-      <ol>
-        <li>${recipes[i].ingredients[0]}</li>
-        <li>${recipes[i].ingredients[1]}</li>
-        <li>${recipes[i].ingredients[2]}</li>
-      </ol>
-    </li>
-    <li>Steps
-      <ul>
-        <li>${recipes[i].steps[0]}</li>
-        <li>${recipes[i].steps[1]}</li>
-        <li>${recipes[i].steps[2]}</li>
-        <li>${recipes[i].steps[3]}</li>
-      </ul>
-    </li>
-  </ul>`;
+      detailHTML = `
+        <div class="detail_for_recipe">
+          <div class="heading_detail">
+            <h2>${recipes[i].title}  <small>${recipes[i].author}</small></h2>
+          </div>
+          <img src="https://bit.ly/2ZXyiKI" alt="">
+          <ul class="list_of_some_info">
+            <li>${recipes[i].difficulty}</li>
+            <li>${recipes[i].timing}</li>
+          </ul>
+          <ul class="list_of_info">
+            <li>Ingredient
+              <ol>
+                <li>${recipes[i].ingredients[0]}</li>
+                <li>${recipes[i].ingredients[1]}</li>
+                <li>${recipes[i].ingredients[2]}</li>
+              </ol>
+            </li>
+            <li>Steps
+              <ul>
+                <li>${recipes[i].steps[0]}</li>
+                <li>${recipes[i].steps[1]}</li>
+                <li>${recipes[i].steps[2]}</li>
+                <li>${recipes[i].steps[3]}</li>
+              </ul>
+            </li>
+          </ul>`;
   };
 
 
-  // contain.insertAdjacentHTML('beforeend', detailHTML)
+// contain.insertAdjacentHTML('beforeend', detailHTML)
 
-  innerModal.innerHTML += detailHTML;
+      innerModal.innerHTML += detailHTML;
   // Add a new class list so that we can creat function to close the the model
-  outerModal.classList.add('open');
-};
+      outerModal.classList.add('open');
+  };
 
 // Close model when we don't need the model
 
-const handleCloseModal = event => {
-  const isOutside = !event.target.closest('.inner-modal');
+  const handleCloseModal = event => {
+    const isOutside = !event.target.closest('.inner-modal');
   if (isOutside) {
     closeModal();
-  }
-};
+    }
+  };
 
-// 
 
-const closeModal = () => {
-  outerModal.classList.remove('open');
-};
 
-const handleEscapeKey = event => {
-  if (event.key === 'Escape') {
-    closeModal();
-  }
-};
+  const closeModal = () => {
+    outerModal.classList.remove('open');
+  };
+
+  const handleEscapeKey = event => {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  };
 
 // Fuction to generate the event Listener of the button of information
 
-const handleClick = (e) => {
+  const handleClick = (e) => {
 
   // This condition for button clicked 
 
-  if (e.target.matches('.more-info')) {
-    detailRecipe();
+    if (e.target.matches('.more-info')) {
+      detailRecipe();
   }
-  if (e.target.matches('.more-info')){
-     const detail = e.target.closest('');
-     const id = Number(detail.dataset.id);
-     const realRecep = rrecipes.find(realRecep => realRecep.id === id)
-  }
+
+    if (e.target.matches('.more-info')){
+      const detail = e.target.closest('.detail_for_recipe');
+      const id = Number(detail.dataset.id);
+      const realRecep = rrecipes.find(realRecep => realRecep.id === id);
+
+      realRecep();
+  };
 }
-const generateButton = document.querySelector('button.generate');
-generateButton.addEventListener('click', renderCard);
-window.addEventListener('click', handleClick);
-outerModal.addEventListener('click', handleCloseModal);
+  const generateButton = document.querySelector('button.generate');
+  generateButton.addEventListener('click', renderCard);
+  window.addEventListener('click', handleClick);
+  outerModal.addEventListener('click', handleCloseModal);
 
